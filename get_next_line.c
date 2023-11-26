b0cde395747f0cccac5aeb479f98c78d61413106
iluvcasca <sntcillian@gmail.com>
2023-11-26 16:45:33 +0000

    pue sa mere

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 15:31:19 by kgriset           #+#    #+#             */
-/*   Updated: 2023/11/25 16:18:39 by kgriset          ###   ########.fr       */
+/*   Updated: 2023/11/26 16:35:35 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,55 +16,63 @@ char *get_next_line(int fd)
 {
     static char remain_buffer[FD_SETSIZE][BUFFER_SIZE];
     char * line;
-    size_t * is_found_and_index;
-    t_buffer_chunk * first_node;
+    t_newline  newline;
+    t_buffer_chunk_control buffer_chunk_control;
     t_buffer_chunk * node;
-    unsigned int node_counter;
-    is_found_and_index = (size_t[2]){0};
-    is_found_and_index = is_line(remain_buffer[fd], is_found_and_index);
-    if (is_found_and_index[0])
-        return (extract_remaining(remain_buffer[fd], is_found_and_index[1]));
-    first_node = add_back_t_buffer_chunk(NULL);
-    node_counter = 1;
-    node = first_node;
-    if (read(fd, node->buffer, BUFFER_SIZE) <= 0)
+    size_t bytes_read;
+
+    if (fd < 0 || fd > FD_SETSIZE)
+        return (NULL);
+    is_line(remain_buffer[fd], &newline, BUFFER_SIZE);
+    if (newline.is_found)
+        return (extract_remaining_line(remain_buffer[fd], newline.index));
+    buffer_chunk_control.first_node = add_back_t_buffer_chunk(NULL);
+    if (!buffer_chunk_control.first_node)
+        return (NULL);
+    buffer_chunk_control.node_counter = 1;
+    node = buffer_chunk_control.first_node;
+    bytes_read = read(fd, node->buffer, BUFFER_SIZE);
+    if (bytes_read <= 0)
     {
-        free_t_buffer_chunk (&first_node);
+        free_t_buffer_chunk (buffer_chunk_control.first_node);
         return (NULL);
     }
-    is_found_and_index = is_line(node->buffer, is_found_and_index);
-    while (!is_found_and_index[0]) 
+    is_line(node->buffer, &newline, bytes_read);
+    while (!newline.is_found || bytes_read < BUFFER_SIZE) 
     {
-        add_back_t_buffer_chunk(node);
-        node_counter++;
+        if (!add_back_t_buffer_chunk(node))
+        {
+            free_t_buffer_chunk(buffer_chunk_control.first_node);
+            return (NULL);
+        }
+        buffer_chunk_control.node_counter++;
         node = node->next;
-        read(fd, node->buffer, BUFFER_SIZE);
-        is_found_and_index = is_line(node->buffer, is_found_and_index);
+        bytes_read = read(fd, node->buffer, BUFFER_SIZE);
+        is_line(node->buffer, &newline, bytes_read);
     }
-        line = build_line(first_node, remain_buffer[fd], node_counter, is_found_and_index[1]); 
-        free_t_buffer_chunk (&first_node);
+        line = build_line(buffer_chunk_control.first_node, remain_buffer[fd], buffer_chunk_control.node_counter, newline.index, bytes_read); 
+        free_t_buffer_chunk (buffer_chunk_control.first_node);
     return (line);
 }
 
-size_t  * is_line (char * buffer, size_t is_found_and_index[2])
+void  is_line (char * buffer, t_newline * newline, size_t bytes_read)
 {
     size_t i;
-    size_t j;
     i = 0;
-    j = 0;
+    newline->is_found = 0;
+    newline->index = 0;
     if (!(*buffer))
-        return is_found_and_index;
-    while (i < BUFFER_SIZE && buffer[i])
+        return ;
+    while (i < bytes_read && buffer[i])
     {
         if (buffer[i] == '\n')
         {
-            is_found_and_index[0] = 1;
-            is_found_and_index[1] = i;
+            newline->is_found = 1;
+            newline->index = i;
             break;
         }
         i++;
     }
-    return is_found_and_index;
 }
 
 t_buffer_chunk *add_back_t_buffer_chunk (t_buffer_chunk *current)
@@ -81,11 +89,11 @@ t_buffer_chunk *add_back_t_buffer_chunk (t_buffer_chunk *current)
     return (last);
 }
 
-void free_t_buffer_chunk(t_buffer_chunk **first)
+void free_t_buffer_chunk(t_buffer_chunk *first)
 {
     t_buffer_chunk * current;
     t_buffer_chunk * next;
-    current = *first;
+    current = first;
     while (current->next)
     {
         next = current->next;
@@ -95,50 +103,48 @@ void free_t_buffer_chunk(t_buffer_chunk **first)
     free (current);
 }
 
-char *build_line(t_buffer_chunk * first, char * remain_buffer, unsigned int node_counter, size_t newline_index)
+char *build_line(t_buffer_chunk * first, char * remain_buffer, unsigned int node_counter, size_t newline_index, size_t bytes_read)
 {
     char * line;
     size_t i;
     size_t j;
     size_t k;
 
-    i = 0;
+    i = -1;
     k = 0;
-    line = malloc(sizeof(char)*((BUFFER_SIZE*(node_counter - 1) + newline_index + 2) + gnl_strlen(remain_buffer)));
-    while (remain_buffer[i])
-    {
+    if (newline_index == 0)
+        newline_index = BUFFER_SIZE - 1;
+    line = malloc(sizeof(char)*((bytes_read*(node_counter - 1) + newline_index + 2) + gnl_strlen(remain_buffer)));
+    if (!line)
+        return (line);
+    while (remain_buffer[++i])
         line [i] = remain_buffer[i];
-        i++;
-    }
     while (node_counter--) 
     {
         j = 0;
-        while ((first->buffer)[j] != '\n' && j < BUFFER_SIZE - 1)
-        {
-            line [i] = (first->buffer)[j];  
-            i++;
-            j++;
-        }
+        while ((first->buffer)[j] != '\n' && j < bytes_read - 1)
+            line [i++] = (first->buffer)[j++];  
         line [i++] = (first->buffer)[j++];
-        while (j<BUFFER_SIZE)
+        while (j<bytes_read)
             remain_buffer[k++] = (first->buffer)[j++];
         remain_buffer[k] = '\0';
         first = first->next;
     }
     line[i] = '\0';
-
     return (line);
 }
 
-char *extract_remaining(char * remain_buffer, size_t newline_index)
+char *extract_remaining_line(char * remain_buffer, size_t newline_index)
 {
+    char * extracted_line;
     size_t i;
     size_t j;
-    char * extracted_line;
 
     i = 0;
     j = 0;
     extracted_line = malloc(sizeof(char)*(newline_index + 2));
+    if (!extracted_line)
+        return (extracted_line);
     while (i != newline_index + 1)
     {
         extracted_line[i] = remain_buffer[i];
@@ -146,13 +152,8 @@ char *extract_remaining(char * remain_buffer, size_t newline_index)
     }
     extracted_line[i] = '\0';
     while (remain_buffer[i])
-    {
-        remain_buffer[j] = remain_buffer[i];
-        j++;
-        i++;
-    }
+        remain_buffer[j++] = remain_buffer[i++];
     remain_buffer[j] = remain_buffer[i];
-    
     return (extracted_line);
 }
 
@@ -161,30 +162,30 @@ size_t gnl_strlen(char * buffer)
     size_t i;
 
     i = 0;
-    while (buffer[i] && i < BUFFER_SIZE)
+    while (buffer[i])
         i++;
     return (i);
 }
 
-#include <stdio.h>
-#include <fcntl.h>
-int main()
- {
-    int fd = open("Quran.txt", O_RDONLY);
-    // printf("%i\n", fd);
-    // char *line = get_next_line(fd);
-    // printf("%s\n", line);
-    // free (line);
-    // printf("%i\n", FD_SETSIZE);
-    int i ;
-    char * line;
-    i = 0;
-    while (i < 14998)
-    {
-        line = get_next_line(fd);
-        printf("%s", line);
-        free (line);
-        i++;
-    }
-    return (1);
-}
+// #include <stdio.h>
+// #include <fcntl.h>
+// int main()
+//  {
+//     int fd = open("Quran.txt", O_RDONLY);
+//     // printf("%i\n", fd);
+//     // char *line = get_next_line(fd);
+//     // printf("%s\n", line);
+//     // free (line);
+//     // printf("%i\n", FD_SETSIZE);
+//     int i ;
+//     char * line;
+//     i = 0;
+//     while (i < 14998)
+//     {
+//         line = get_next_line(-5);
+//         printf("%s", line);
+//         free (line);
+//         i++;
+//     }
+//     return (1);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 15:22:00 by kgriset           #+#    #+#             */
-/*   Updated: 2023/11/25 15:12:49 by kgriset          ###   ########.fr       */
+/*   Updated: 2023/11/26 16:19:46 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,27 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/select.h>
+
 typedef struct s_buffer_chunk {
     struct s_buffer_chunk *next;
     char buffer[BUFFER_SIZE];
 } t_buffer_chunk;
+
+typedef struct s_buffer_chunk_control {
+    struct s_buffer_chunk * first_node;
+    unsigned int node_counter;
+} t_buffer_chunk_control;
+
+typedef struct s_newline {
+    unsigned int is_found;
+    size_t index;
+} t_newline;
+
 char *get_next_line(int fd);
-size_t  * is_line (char * buffer, size_t is_found_and_index[2]);
-char *build_line(t_buffer_chunk * first, char * remain_buffer, unsigned int node_counter, size_t newline_index);
+void  is_line (char * buffer, t_newline * newline, size_t bytes_read);
+char *build_line(t_buffer_chunk * first, char * remain_buffer, unsigned int node_counter, size_t newline_index, size_t bytes_read);
 t_buffer_chunk *add_back_t_buffer_chunk (t_buffer_chunk *current);
-void free_t_buffer_chunk(t_buffer_chunk **first);
+void free_t_buffer_chunk(t_buffer_chunk *first);
 size_t gnl_strlen(char * buffer);
-char *extract_remaining(char * remain_buffer, size_t newline_index);
+char *extract_remaining_line(char * remain_buffer, size_t newline_index);
 #endif
